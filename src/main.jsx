2e2de2b7326536a08b5226dcd1a6b9fa23dc0fac
Soumilmittal <mittasoumill@gmail.com>
2026-02-21 01:14:12 +0530

    made team.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Hero from './hero'
+import Team from './team'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
     <Hero />
+    <Team />
   </StrictMode>,
 )
