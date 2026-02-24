8ba3c23f535f48c59660b3d10833ff752c5c347d
Aditya Garg <adityagarggzb@gmail.com>
2026-02-24 13:10:53 +0530

    small changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Hero from './hero'
 import Team from './team'
-import Event from './event'
+import Event from './event1'
 import Gallery from './gallery'     
 
 createRoot(document.getElementById('root')).render(
